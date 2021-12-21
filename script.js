446ca7d688fb1972eda7ee2fbecb3c0f9f3c32d5
Laraib39 <sheikhlareb9d@gmail.com>
2021-12-21 16:21:22 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+
+    // initiation of wow
+    new WOW().init();
+
+    // carousel applied to team section 
+    $('.team .owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        dots: true,
+        nav: false,
+        responsiveClass: true,
+        responsive:{
+            0:{
+                items: 1
+            }, 
+            600:{
+                items: 2
+            },
+            1000:{
+                items: 3
+            }
+        }
+    });
+
+    // carousel applied to Testimonial section
+    $('.testimonial .owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        dots: true,
+        nav: false,
+        items: 1
+    });
+
+});
